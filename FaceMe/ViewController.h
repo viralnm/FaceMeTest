233c1dd62db59778559fba3846682b6a33f9967b
Viral Mithani <viral@itoneclick.com>
2017-09-14 11:59:30 +0530

    Initial Commit

---
+++ FaceMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceMe
+//
+//  Created by Oneclick IT Solution on 9/14/17.
+//  Copyright © 2017 One Click IT Consultancy Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FaceMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FaceMe
+//
+//  Created by Oneclick IT Solution on 9/14/17.
+//  Copyright © 2017 One Click IT Consultancy Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
